23e24bb141881b778ff37e33facf363df013c15b
i-am-knots <knots@tuta.io>
2021-11-10 22:04:49 -0500

    Add api

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # keras-style-transfer
 Repository for deploying a Keras Tensorflow nural style transfer implementation to GCP
+
+## Render Frame
+Microservice to render a single frame with an api to listen for paramaters
+
+### Example API Request
+```
+HTTP GET - localhost:8080/render?bucket=gcs-bucket-name&style=style.jpg&source=test.png&iterations=10
+```
+
+### Test locally
+```
+cd frame_render
+python3 render-api.py
+```
---
+++ frame_render/render-api.py
@@ -0,0 +1,34 @@
+from frame import render_frame
+from flask import Flask, json, request
+
+api = Flask(__name__)
+
+def test_render_frame(bucket, source, style):
+    print("this is a mock render")
+    print("the bucket is", bucket)
+    print("the source is", source)
+    print("the style is", style)
+
+
+@api.route('/render', methods=['GET'])
+def render_handler():
+    bucket     = request.args.get('bucket')
+    source     = request.args.get('source')
+    style      = request.args.get('style')
+    size       = request.args.get('size')
+    iterations = request.args.get('iterations')
+    
+    render_frame(bucket, source, style, size, iterations)
+    
+    return "Frame render complete"
+
+@api.route('/render-test', methods=['GET'])
+def get_render_test():
+    bucket = request.args.get('bucket')
+    source = request.args.get('source')
+    style  = request.args.get('style')
+    test_render_frame(bucket, source, style)
+    return "Done"
+
+if __name__ == '__main__':
+    api.run(port=8080) 
