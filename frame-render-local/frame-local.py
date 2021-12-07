8f0622b683d922771d8ea55b206467d04d4b4c20
i-am-knots <knots@tuta.io>
2021-12-06 22:02:10 -0500

    use tensorflow module for faster style transfer

--- frame-render-local/frame-local.py
+++ frame-render-local/frame-local.py
@@ -1,176 +1,102 @@
+import cv2
+import os
+import glob
+
+import IPython.display as display
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (12, 12)
+mpl.rcParams['axes.grid'] = False
+
 import numpy as np
+import PIL.Image
+import time
+import functools
+
+import tensorflow_hub as hub
 import tensorflow as tf
+# Load compressed models from tensorflow_hub
+os.environ['TFHUB_MODEL_LOAD_FORMAT'] = 'COMPRESSED'
 from tensorflow import keras
 from tensorflow.keras.applications import vgg19
 
-base_image_path =  "source-image/test.png"
-style_reference_image_path = "style-images/style.jpg"
-iterations = 100
+def render_frame():
 
-result_prefix = "generated"
+    def tensor_to_image(tensor):
+        tensor = tensor*255
+        tensor = np.array(tensor, dtype=np.uint8)
+        if np.ndim(tensor)>3:
+            assert tensor.shape[0] == 1
+            tensor = tensor[0]
+        return PIL.Image.fromarray(tensor)
 
-# Weights of the different loss components
-total_variation_weight = 1e-6
-style_weight = 1e-6
-content_weight = 2.5e-5
+    # Load content and style images (see example in the attached colab).
+    style_image = plt.imread("style-images/style.jpg")
+    print("SET IMAGES SUCCESS")
+    # Convert to float32 numpy array, add batch dimension, and normalize to range [0, 1]. Example using numpy:
+    
+    style_image = style_image.astype(np.float32)[np.newaxis, ...] / 255.
+    # Optionally resize the images. It is recommended that the style image is about
+    # 256 pixels (this size was used when training the style transfer network).
+    # The content image can be any size.
+    style_image = tf.image.resize(style_image, (256, 256))
 
-# Dimensions of the generated picture.
-width, height = keras.preprocessing.image.load_img(base_image_path).size
-img_nrows = int(720)
-img_ncols = int(width * img_nrows / height)
+    # Load image stylization module.
+    hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
 
-print("SET VARIABLES SUCCESS")
-
-def preprocess_image(image_path):
-    # Util function to open, resize and format pictures into appropriate tensors
-    img = keras.preprocessing.image.load_img(
-        image_path, target_size=(img_nrows, img_ncols)
-    )
-    img = keras.preprocessing.image.img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    img = vgg19.preprocess_input(img)
-    return tf.convert_to_tensor(img)
-
-
-def deprocess_image(x):
-    # Util function to convert a tensor into a valid image
-    x = x.reshape((img_nrows, img_ncols, 3))
-    # Remove zero-center by mean pixel
-    x[:, :, 0] += 103.939
-    x[:, :, 1] += 116.779
-    x[:, :, 2] += 123.68
-    # 'BGR'->'RGB'
-    x = x[:, :, ::-1]
-    x = np.clip(x, 0, 255).astype("uint8")
-    return x
-
-# The gram matrix of an image tensor (feature-wise outer product)
+    # Stylize image.
+    for file in glob.glob("source-frames/*.jpg"):
+        print(file)
+        content_image = plt.imread(file)
+        content_image = content_image.astype(np.float32)[np.newaxis, ...] / 255.
+        outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
+        output_name = "output/" + file
+        tensor_to_image(outputs[0]).save(output_name)
 
 
-def gram_matrix(x):
-    x = tf.transpose(x, (2, 0, 1))
-    features = tf.reshape(x, (tf.shape(x)[0], -1))
-    gram = tf.matmul(features, tf.transpose(features))
-    return gram
+
+def make_frames():
+    # Get the source video file from GCS
+ 
+    # Make frames from video with set FPS
+    video_capture = cv2.VideoCapture("test.mp4")
+    video_capture.set(cv2.CAP_PROP_FPS, 30)
+
+    saved_frame_name = 0
+
+    while video_capture.isOpened():
+        frame_is_read, frame = video_capture.read()
+
+        if frame_is_read:
+            cv2.imwrite(f"source-frames/frame{str(saved_frame_name)}.jpg", frame)
+            
+            saved_frame_name += 1
+
+        else:
+            print("Could not read the frame.")
+            return
+
+def build_video():
+    img_array = []
+    for filename in glob.glob('output/source-frames/*.jpg'):
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width,height)
+        img_array.append(img)
+
+    out = cv2.VideoWriter('output/test.avi',cv2.VideoWriter_fourcc(*'DIVX'), 30, size)
+ 
+    for i in range(len(img_array)):
+        out.write(img_array[i])
+    out.release()
+
+def main():
+    os.mkdir("source-frames")
+    os.mkdir("output")
+    os.mkdir("output/source-frames")
+    make_frames()
+    render_frame()
+    build_video()
 
 
-# The "style loss" is designed to maintain
-# the style of the reference image in the generated image.
-# It is based on the gram matrices (which capture style) of
-# feature maps from the style reference image
-# and from the generated image
-
-
-def style_loss(style, combination):
-    S = gram_matrix(style)
-    C = gram_matrix(combination)
-    channels = 3
-    size = img_nrows * img_ncols
-    return tf.reduce_sum(tf.square(S - C)) / (4.0 * (channels ** 2) * (size ** 2))
-
-
-# An auxiliary loss function
-# designed to maintain the "content" of the
-# base image in the generated image
-
-
-def content_loss(base, combination):
-    return tf.reduce_sum(tf.square(combination - base))
-
-
-# The 3rd loss function, total variation loss,
-# designed to keep the generated image locally coherent
-
-
-def total_variation_loss(x):
-    a = tf.square(
-        x[:, : img_nrows - 1, : img_ncols - 1, :] - x[:, 1:, : img_ncols - 1, :]
-    )
-    b = tf.square(
-        x[:, : img_nrows - 1, : img_ncols - 1, :] - x[:, : img_nrows - 1, 1:, :]
-    )
-    return tf.reduce_sum(tf.pow(a + b, 1.25))
-
-# Build a VGG19 model loaded with pre-trained ImageNet weights
-model = vgg19.VGG19(weights="imagenet", include_top=False)
-
-print("BUILT VGG19 MODEL")
-
-# Get the symbolic outputs of each "key" layer (we gave them unique names).
-outputs_dict = dict([(layer.name, layer.output) for layer in model.layers])
-
-
-# Set up a model that returns the activation values for every layer in
-# VGG19 (as a dict).
-feature_extractor = keras.Model(inputs=model.inputs, outputs=outputs_dict)
-print("SET UP MODEL OKAY")
-# List of layers to use for the style loss.
-style_layer_names = [
-    "block1_conv1",
-    "block2_conv1",
-    "block3_conv1",
-    "block4_conv1",
-    "block5_conv1",
-]
-
-# The layer to use for the content loss.
-content_layer_name = "block5_conv2"
-
-
-def compute_loss(combination_image, base_image, style_reference_image):
-    input_tensor = tf.concat(
-        [base_image, style_reference_image, combination_image], axis=0
-    )
-    features = feature_extractor(input_tensor)
-
-    # Initialize the loss
-    loss = tf.zeros(shape=())
-
-    # Add content loss
-    layer_features = features[content_layer_name]
-    base_image_features = layer_features[0, :, :, :]
-    combination_features = layer_features[2, :, :, :]
-    loss = loss + content_weight * content_loss(
-        base_image_features, combination_features
-    )
-    # Add style loss
-    for layer_name in style_layer_names:
-        layer_features = features[layer_name]
-        style_reference_features = layer_features[1, :, :, :]
-        combination_features = layer_features[2, :, :, :]
-        sl = style_loss(style_reference_features, combination_features)
-        loss += (style_weight / len(style_layer_names)) * sl
-
-    # Add total variation loss
-    loss += total_variation_weight * total_variation_loss(combination_image)
-    return loss
-
-@tf.function
-def compute_loss_and_grads(combination_image, base_image, style_reference_image):
-    with tf.GradientTape() as tape:
-        loss = compute_loss(combination_image, base_image, style_reference_image)
-    grads = tape.gradient(loss, combination_image)
-    return loss, grads
-
-optimizer = keras.optimizers.SGD(
-    keras.optimizers.schedules.ExponentialDecay(
-        initial_learning_rate=100.0, decay_steps=100, decay_rate=0.96
-    )
-)
-
-print("STARTING IMAGE PREPROCESSING")
-base_image = preprocess_image(base_image_path)
-style_reference_image = preprocess_image(style_reference_image_path)
-combination_image = tf.Variable(preprocess_image(base_image_path))
-print("FINISHED IMAGE PREPROCESSING")
-
-for i in range(1, int(iterations) + 1):
-    loss, grads = compute_loss_and_grads(
-        combination_image, base_image, style_reference_image
-    )
-    optimizer.apply_gradients([(grads, combination_image)])
-    print("Iteration %d: loss=%.2f" % (i, loss))
-    if i % 5 == 0:
-        img = deprocess_image(combination_image.numpy())
-        fname = "output/" + result_prefix + "_at_iteration_%d.png" % i
-        keras.preprocessing.image.save_img(fname, img)
+main()
